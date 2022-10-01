898d59c7ebb8a26b43319113d3d86aaed0a0a14c
amrit <info.amritpant@gmail.com>
2022-10-01 10:30:12 +0545

    Created Mongodb Model For Article

---
+++ models/articleModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please Provide Title for an Article!'],
+    minLength: [8, 'A title cannot be less than 8 character'],
+    maxLength: [50, 'A title cannot exceed 50 Characters'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please Provide Description for an Article!'],
+    minLength: [120, 'A title cannot be less than 8 character'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Please Provide the Cateogry of the Article'],
+    enum: ['tech', 'productivity', 'lifestyle', 'education'],
+  },
+  thumbnail: {
+    type: String,
+  },
+  publishedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  length: {
+    type: Number,
+    required: [true, 'Please Provide Length of the Article'],
+    min: [1, 'Length Cannot be less than 1 Mintues'],
+    max: [60, 'Length Cannot be greater than 60 Minutes'],
+  },
+  tags: {
+    type: String,
+    required: false,
+    enum: ['trending'],
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const ArticleModel = mongoose.model('Article', articleSchema);
+module.exports = ArticleModel;
