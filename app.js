c0f14d4d11cc937fa9a0cb71966b72dd502c21d1
amrit <info.amritpant@gmail.com>
2022-10-01 10:32:29 +0545

    Mounted the articleRouter to the main Router

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const articleRouter = require('./routes/articleRoutes');
 
 const app = express();
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ data: 'thanks for contacting' });
-});
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+// Routers
+app.use('/api/v1/article', articleRouter);
 
 module.exports = app;
