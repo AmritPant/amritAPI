fdc4ccb598742c1bad92470c64c6ca5abaa171fb
amrit <info.amritpant@gmail.com>
2022-10-02 13:33:33 +0545

    Wrraped controller functions with catchAsync functions (to avoid try catch block)

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,7 +1,8 @@
 const Article = require('../models/articleModel');
 const ApiFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getAllArticles = async (req, res) => {
+exports.getAllArticles = catchAsync(async (req, res) => {
   const query = Article.find({});
 
   const apiFeatures = new ApiFeatures(query, req.query)
@@ -15,14 +16,10 @@ exports.getAllArticles = async (req, res) => {
   res
     .status(200)
     .json({ status: 'success', count: articles.length, data: articles });
-};
+});
 
-exports.addNewArticle = async (req, res) => {
-  try {
-    const article = req.body;
-    const response = await Article.create(article);
-    res.status(201).json(response);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
+exports.addNewArticle = catchAsync(async (req, res) => {
+  const article = req.body;
+  const response = await Article.create(article);
+  res.status(201).json(response);
+});
