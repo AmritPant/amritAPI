5d7940d24bc5c783b51a251e989db5b66ea5f86f
amrit <info.amritpant@gmail.com>
2022-10-02 18:25:35 +0545

    Added getOne, updateOne, deleteOne feature

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,6 +1,7 @@
 const Article = require('../models/articleModel');
 const ApiFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const CustomError = require('../utils/CustomError');
 
 exports.getAllArticles = catchAsync(async (req, res) => {
   const query = Article.find({});
@@ -22,4 +23,42 @@ exports.addNewArticle = catchAsync(async (req, res) => {
   const article = req.body;
   const response = await Article.create(article);
   res.status(201).json(response);
+});
+
+exports.getOneArticle = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const article = await Article.findById(id);
+  if (!article)
+    return next(
+      new CustomError("The Article You are searching for doesn't exist", 404)
+    );
+  res.status(200).json({ status: 'sucess', data: article });
+});
+
+exports.updateArticle = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const article = await Article.findById(id);
+  if (!article)
+    return next(
+      new CustomError("The Article You are searching for doesn't exist", 404)
+    );
+  const data = await Article.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    returnDocument: 'after',
+  });
+
+  res.status(200).json({ status: 'sucess', data: data });
+});
+
+exports.deleteArticle = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const article = await Article.findById(id);
+
+  if (!article)
+    return next(
+      new CustomError("The Article You are searching for doesn't exist", 404)
+    );
+
+  await Article.findByIdAndDelete(id);
+  res.status(200).json({ status: 'sucess', data: null });
 });
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -7,4 +7,9 @@ Router.route('/')
   .get(articleController.getAllArticles)
   .post(articleController.addNewArticle);
 
+Router.route('/:id')
+  .get(articleController.getOneArticle)
+  .patch(articleController.updateArticle)
+  .delete(articleController.deleteArticle);
+
 module.exports = Router;
