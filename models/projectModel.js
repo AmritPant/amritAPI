ddbda0c098da798d868d4d4d53da4acc4bef8f18
amrit <info.amritpant@gmail.com>
2022-10-03 11:48:31 +0545

    Created a Project ModeL

---
+++ models/projectModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please Provide the title of the Project'],
+    maxLength: 60,
+    minLength: 4,
+    unique: [true, 'A title of Project Should be Unique'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please Provide the title of the Project'],
+    minLength: 100,
+  },
+  platform: {
+    type: String,
+    required: [true, 'Please Provide the platform of the Project'],
+    enum: {
+      values: ['mobile', 'dekstop', 'web', 'server'],
+      message: '{VALUE} is not the correct Platform',
+    },
+  },
+  completedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  totalTime: {
+    type: Number,
+    min: [1, 'Project cannot be completed in less than 1day'],
+    required: [true, 'Please Provide the platform of the Project'],
+  },
+  thumbnail: {
+    type: String,
+  },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
