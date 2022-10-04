aed88a509d2a7ce0a9f2b2906f1c63e091ce24d6
amrit <info.amritpant@gmail.com>
2022-10-04 15:13:45 +0545

    Added a protected function which will protect important routes

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -52,3 +52,24 @@ module.exports.getOne = Model =>
       );
     res.status(200).json({ status: 'sucess', data: document });
   });
+
+module.exports.protect = (req, res, next) => {
+  const { API_KEY } = req.query;
+  if (!API_KEY)
+    return next(
+      new CustomError(
+        'API KEY is not present in request | Please Provide Correc API KEY',
+        401
+      )
+    );
+
+  if (API_KEY !== process.env.API_KEY)
+    next(new CustomError('Invalid API_KEY', 400));
+
+  next();
+};
+
+module.exports.checkIP = (req, res, next) => {
+  console.log(req);
+  next();
+};
